0768fd3445c40c3f338da75173b453bc7e24b523
Fathyy <fathiabdi254@gmail.com>
2023-10-21 20:32:16 +0300

    created the includes section

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,10 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+<title><?php echo $pageTitle?></title>
+<!-- bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<!-- fontawesome -->
+<script src="https://kit.fontawesome.com/ed20622ed8.js" crossorigin="anonymous"></script>
+
+<!-- full calender js -->
+<link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js">
--- includes/template.php
+++ includes/template.php
@@ -12,7 +12,55 @@ $self = WEB_ROOT . 'index.php';
 <head>
     <?php include 'header.php'?>
 </head>
-<body>
+<body class="skin-blue sidebar-mini">
+    <div class="wrapper">
+        <header class="main-header">
+            <a href="<?php echo WEB_ROOT?>" class="logo">
+                <span class="logo-lg"><b>Event Management </b></span>
+            </a>
+
+            <nav class="navbar navbar-static-top" role="navigation">
+                <?php include 'nav.php'?>
+            </nav>
+        </header>
+
+
+        <!-- letf aside column that contains logo and sidebar -->
+        <aside class="main-sidebar">
+            <?php include 'sidebar.php'?>
+        </aside>
+
+
+        <!-- page content -->
+        <div class="content-wrapper">
+            <section class="section-header">
+                <h1>Event Management <small>manage your events easily</small></h1>
+                <ol class="breadcrumb">
+                    <li><a href=""><i class="fa fa-dashboard"></i>Home</a></li>
+                    <li class="active">Calender</li>
+                </ol>
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- include notifications page -->
+                        <?php include 'messages.php'?>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <?php require_once $content?>
+                </div>
+            </section>
+        </div>
+
+        <!-- footer -->
+        <footer class="main-footer">
+            <?php include 'footer.php'?>
+        </footer>
+    </div>
     
 </body>
 </html>
